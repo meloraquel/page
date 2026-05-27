949c18c1bee4073d52fb7f40f4ffca6b6913311e
meloraquel <raquelmelo0823@gmail.com>
2026-05-27 19:30:52 -0300

    Update aula.py

--- Unidade 6 - tarefa 6/aula.py
+++ Unidade 6 - tarefa 6/aula.py
@@ -1,9 +1,36 @@
-nomes = []
+#AULA27/05/26
+
+nomes = [,aria, joao, josé, leticia]
 # acessar um incice do elemento(índice)
 print(nomes[1]
 #adicionar elemento no final
 nomes.append("Kelly")
+      
 #remove elementos      
 nomes.pop("Kelly")
+      
 # adicionar um elemento pelo índice - Parâmetros: (índice do elemento, valor
 nomes.insert(0,"raquel")
+      
+#remover os nomes pelo índice
+nomes.pop(1)
+
+#procurar os nomes em uma lista
+print("Douglas" in nomes)
+
+#ordenar os vetores é basicamente criar um novo vetor ordenado
+nomes.sort()
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
