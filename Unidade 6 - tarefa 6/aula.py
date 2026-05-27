2e802fab6496314f59ebcd9e745a9cc164d4d746
meloraquel <raquelmelo0823@gmail.com>
2026-05-27 19:21:08 -0300

    Implement list operations for names
    Added list operations for managing names.

--- Unidade 6 - tarefa 6/aula.py
+++ Unidade 6 - tarefa 6/aula.py
@@ -1,1 +1,9 @@
-
+nomes = []
+# acessar um incice do elemento(índice)
+print(nomes[1]
+#adicionar elemento no final
+nomes.append("Kelly")
+#remove elementos      
+nomes.pop("Kelly")
+# adicionar um elemento pelo índice - Parâmetros: (índice do elemento, valor
+nomes.insert(0,"raquel")
